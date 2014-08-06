da85db03d05f33e8a4c9434ea8a43762d8f5002c
unknown <Zoeb Chhatriwala>
2014-08-06 20:24:17 +0530

    first commit

---
+++ WebCreator/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WebCreator
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.input = new System.Windows.Forms.TextBox();
+            this.tcontrol = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.web = new System.Windows.Forms.WebBrowser();
+            this.open = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.submit = new System.Windows.Forms.Button();
+            this.about = new System.Windows.Forms.Button();
+            this.tcontrol.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.input.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.MaxLength = 52000;
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input.Size = new System.Drawing.Size(453, 434);
+            this.input.TabIndex = 0;
+            this.input.WordWrap = false;
+            this.input.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.input_KeyPress);
+            // 
+            // tcontrol
+            // 
+            this.tcontrol.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tcontrol.Controls.Add(this.tabPage1);
+            this.tcontrol.Location = new System.Drawing.Point(471, 12);
+            this.tcontrol.Name = "tcontrol";
+            this.tcontrol.SelectedIndex = 0;
+            this.tcontrol.Size = new System.Drawing.Size(386, 434);
+            this.tcontrol.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.web);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(378, 408);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Output";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // web
+            // 
+            this.web.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.web.Location = new System.Drawing.Point(3, 3);
+            this.web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web.Name = "web";
+            this.web.ScriptErrorsSuppressed = true;
+            this.web.Size = new System.Drawing.Size(372, 399);
+            this.web.TabIndex = 0;
+            // 
+            // open
+            // 
+            this.open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.open.Location = new System.Drawing.Point(12, 452);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(113, 23);
+            this.open.TabIndex = 3;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // clear
+            // 
+            this.clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clear.Location = new System.Drawing.Point(250, 452);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(113, 23);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.save.Location = new System.Drawing.Point(131, 452);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(113, 23);
+            this.save.TabIndex = 5;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.refresh.Location = new System.Drawing.Point(625, 452);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(113, 23);
+            this.refresh.TabIndex = 6;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // submit
+            // 
+            this.submit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.submit.Location = new System.Drawing.Point(369, 452);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(250, 23);
+            this.submit.TabIndex = 7;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // about
+            // 
+            this.about.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.about.Location = new System.Drawing.Point(744, 452);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(113, 23);
+            this.about.TabIndex = 8;
+            this.about.Text = "About";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 485);
+            this.Controls.Add(this.about);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.tcontrol);
+            this.Controls.Add(this.input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.Text = "WebCloud";
+            this.tcontrol.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TabControl tcontrol;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser web;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Button about;
+    }
+}
+
---
+++ WebCreator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebCreator
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        
+        
+       
+        
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            input.Text = "";
+        }
+
+        
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|html files (*.html)|*.html|css files (*.css)|*.css|php files (*.php)|*.php|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            input.Text = File.ReadAllText(openFileDialog1.FileName);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            display();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+             
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|html files (*.html)|*.html|css files (*.css)|*.css|php files (*.php)|*.php|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+               File.WriteAllText(saveFileDialog1.FileName, input.Text);
+            }
+         }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            display();
+        }
+        private void display()
+        {
+
+            web.DocumentText = input.Text;
+            if (web.DocumentTitle != "")
+            {
+                
+                tabPage1.Text = web.DocumentTitle;
+                
+            }
+            else
+            {
+                tabPage1.Text = "Output";
+            }
+        }
+
+        private void about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("WebCloud is a Live Html Viewer for Developing Html Websites.\nApplication Developed By: Zoeb Chhatriwala\nHelp:-\n1. Enter Full Path of any File e.g: c:\\folder\\img.jpg\n2. Use Ctrl+r|Submit to Run");
+        }
+
+        private void input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int key = e.KeyChar;
+            if (key==1)
+            {
+                input.SelectAll();
+            }
+           else if (key==18)
+            {
+                display();
+            }
+        }
+        
+    }
+    }
+
